5d3585d5e284af8ceff00c22de8d8c5ad3367594
Aaron Geiger <aaron@geigr.dev>
2023-02-07 19:23:27 +0100

    Make icons monochrome

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -37,8 +37,13 @@ local ICONS = {
     Text          = ""
 }
 
-local cmp = require('cmp')
+-- Make icons monochrome
+require 'nvim-web-devicons'.setup({
+    color_icons = false,
+    default     = true
+})
 
+local cmp = require('cmp')
 cmp.setup {
     view = {
         entries = { name = 'custom' }
