b5102d8210b26f0537b7f3109514bbbc5a78e16f
Aaron Geiger <aaron@geigr.dev>
2023-02-23 18:30:29 +0100

    Add ignore paths for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,4 @@
-local builtin = require'telescope.builtin';
+local builtin = require 'telescope.builtin';
 
 vim.keymap.set("n", "<leader>ö", ':Telescope fd<CR>')
 vim.keymap.set("n", "<leader>ä", builtin.git_files)
@@ -9,11 +9,18 @@ vim.keymap.set("n", "<leader>p", builtin.commands)
 
 -- This is your opts table
 require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {}
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+        }
+    },
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            "target",
+            "dist"
+        }
     }
-  }
 }
 
 require("telescope").load_extension("ui-select")
