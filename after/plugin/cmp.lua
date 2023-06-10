4b6031da707c0ee7e17e5726a2ee153abbb071bd
Aaron Geiger <aaron@geigr.dev>
2023-06-10 21:55:26 +0200

    Fix complete on <CR>

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,42 +1,3 @@
-local ICONS = {
-    Method        = "",
-    Function      = "",
-    Constructor   = "",
-    Field         = "",
-    Variable      = "",
-    Module        = "",
-    Unit          = "",
-    Value         = "",
-    Keyword       = "",
-    Snippet       = "",
-    Color         = "",
-    File          = "",
-    Reference     = "",
-    Folder        = "",
-    EnumMember    = "",
-    Constant      = "",
-    Struct        = "",
-    Event         = "",
-    Operator      = "",
-    TypeParameter = "",
-    Package       = "",
-    Class         = "",
-    Property      = "",
-    Enum          = "",
-    Interface     = "",
-    String        = "",
-    Number        = "",
-    Boolean       = "",
-    Array         = "",
-    Object        = "",
-    Key           = "",
-    Null          = "",
-    TypeAlias     = "",
-    Parameter     = "",
-    Macro         = "",
-    Text          = ""
-}
-
 -- Make icons monochrome
 require 'nvim-web-devicons'.setup({
     color_icons = false,
@@ -47,20 +8,5 @@ local cmp = require('cmp')
 cmp.setup {
     view = {
         entries = { name = 'custom' }
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            -- Kind icons
-            vim_item.kind = string.format('%s [%s]', ICONS[vim_item.kind], vim_item.kind)
-            -- Source
-            vim_item.menu = ({
-                    nvim_lsp      = "[LSP]",
-                    luasnip       = "[LuaSnip]",
-                    nvim_lua      = "[Lua]",
-                    latex_symbols = "[LaTeX]",
-                    buffer        = "[Buffer]",
-                })[entry.source.name]
-            return vim_item
-        end
     },
 }
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,6 +20,7 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>']  = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
 })
 
