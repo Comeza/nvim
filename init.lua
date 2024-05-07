b1b0404c298e623d7de45d2b866549fcf44270dc
Aaron Geiger <aaron@geigr.dev>
2024-05-07 17:31:41 +0200

    Allow environment to set colorscheme

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require 'config.options'
 require 'config.keymap'
 require 'config.lazy'
 require 'config.neovide'
+
+vim.cmd.colorscheme(os.getenv("NVIM_COLORSCHEME") or "habamax")
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,6 @@
 return {
     { "aktersnurra/no-clown-fiesta.nvim", priority = 1000 },
-    { "rose-pine/neovim",                 name = "rose-pine", config = function() vim.cmd.colorscheme('rose-pine-main') end },
+    { "rose-pine/neovim",                 name = "rose-pine", priority = 1000 },
     { "ellisonleao/gruvbox.nvim",         priority = 1000 },
     { "rebelot/kanagawa.nvim",            priority = 1000 },
     { "comfysage/evergarden",             priority = 1000 }
